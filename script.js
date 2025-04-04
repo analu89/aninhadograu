2376ae468a9614770592da058feaa01049e95f6d
analu89 <161457421+analu89@users.noreply.github.com>
2025-04-04 12:42:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('#1', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
